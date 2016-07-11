1a5a197c6b72176a2ce54792bdd31c5cd7d99f92
wenzs <1104793497@qq.com>
2016-07-11 15:20:04 +0800

    Initial commit

---
+++ app/src/main/java/com/wenzs/putaomovie/CinemaMoviePrice.java
@@ -0,0 +1,33 @@
+package com.wenzs.putaomovie;
+
+/**
+ * Created by Administrator on 2016/6/29.
+ */
+public class CinemaMoviePrice {
+    private long ptMovieId;
+    private long moviePrice;
+
+    public long getMoviePrice() {
+        return moviePrice;
+    }
+
+    public void setMoviePrice(long moviePrice) {
+        this.moviePrice = moviePrice;
+    }
+
+    public long getPtMovieId() {
+        return ptMovieId;
+    }
+
+    public void setPtMovieId(long ptMovieId) {
+        this.ptMovieId = ptMovieId;
+    }
+
+    @Override
+    public String toString() {
+        return "CinemaMoviePrice{" +
+                "ptMovieId=" + ptMovieId +
+                ", moviePrice=" + moviePrice +
+                '}';
+    }
+}
